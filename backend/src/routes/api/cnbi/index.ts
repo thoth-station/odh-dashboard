6f527d529a0d6709acb70fc30656e07716d08cdc
Shreekara <sshreeka@redhat.com>
2022-11-16 14:13:08 -0500

    Cleanup
    Signed-off-by: Shreekara <sshreeka@redhat.com>

--- backend/src/routes/api/cnbi/index.ts
+++ backend/src/routes/api/cnbi/index.ts
@@ -1,52 +1,13 @@
 import { FastifyInstance, FastifyReply, FastifyRequest } from 'fastify';
 import { ImageType } from '../../../types';
-import { postCNBI, deleteCNBICrd, getImageList, updateImage } from './cnbiUtils';
+import { postCNBI, deleteCNBICrd } from './cnbiUtils';
 import { secureAdminRoute, secureRoute } from '../../../utils/route-security';
 
 export default async (fastify: FastifyInstance): Promise<void> => {
-  fastify.get(
-    '/:type',
-    secureRoute(fastify)(async (request: FastifyRequest, reply: FastifyReply) => {
-      let labels = {};
-      const params = request.params as { type: ImageType };
-      if (params.type === 'byon') {
-        labels = {
-          'app.kubernetes.io/created-by': 'byon',
-        };
-      }
-      // Additional types like: Other and Jupyter can be added to specify
-      else {
-        labels = {
-          'opendatahub.io/notebook-image': 'true',
-        };
-      }
-      return getImageList(fastify, labels)
-        .then((res) => {
-          return res;
-        })
-        .catch((res) => {
-          reply.send(res);
-        });
-    }),
-  );
-
   fastify.delete(
     '/:crd',
     secureAdminRoute(fastify)(async (request: FastifyRequest, reply: FastifyReply) => {
       return deleteCNBICrd(fastify, request)
-        .then((res) => {
-          return res;
-        })
-        .catch((res) => {
-          reply.send(res);
-        });
-    }),
-  );
-
-  fastify.put(
-    '/:image',
-    secureAdminRoute(fastify)(async (request: FastifyRequest, reply: FastifyReply) => {
-      return updateImage(fastify, request)
         .then((res) => {
           return res;
         })
