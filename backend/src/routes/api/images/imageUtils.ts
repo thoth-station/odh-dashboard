108cffb8141a12c71dd874dd2e413d9f9f53b1af
Pep Turró Mauri <pep@redhat.com>
2022-11-30 14:41:09 +0100

    Rename Custom Notebook Image to Custom Runtime Environment
    Also CNBi to CRE.
    
    Also: switch from created-by to part-of for labels
    
    Signed-off-by: Pep Turró Mauri <pep@redhat.com>

--- backend/src/routes/api/images/imageUtils.ts
+++ backend/src/routes/api/images/imageUtils.ts
@@ -1,308 +1,308 @@
 import { IMAGE_ANNOTATIONS } from '../../../utils/constants';
 import { convertLabelsToString } from '../../../utils/componentUtils';
 import {
-  ImageStreamTag,
-  ImageTagInfo,
-  ImageInfo,
-  ImageStream,
-  TagContent,
-  KubeFastifyInstance,
-  CREImageStreamDetails,
-  CREPackageAnnotation,
-  CREImageStreamPhase,
-  CREImageStreamUpdateRequest,
+    ImageStreamTag,
+    ImageTagInfo,
+    ImageInfo,
+    ImageStream,
+    TagContent,
+    KubeFastifyInstance,
+    CREImageStreamDetails,
+    CREPackageAnnotation,
+    CREImageStreamPhase,
+    CREImageStreamUpdateRequest,
 } from '../../../types';
 import { FastifyRequest } from 'fastify';
 import createError from 'http-errors';
 
 export const getImageList = async (
-  fastify: KubeFastifyInstance,
-  labels: { [key: string]: string },
+    fastify: KubeFastifyInstance,
+    labels: { [key: string]: string },
 ): Promise<CREImageStreamDetails[] | ImageInfo[]> => {
-  const imageStreamList = await Promise.resolve(getImageStreams(fastify, labels));
-  // Return CRE structured response if CRE label is included
-  if (labels['app.kubernetes.io/created-by'] === 'cpe-_a-meteor.zone-CNBi-v0.1.0') {
-    const CREImageList: CREImageStreamDetails[] = [
-      ...imageStreamList.map((is) => mapImageStreamToCREImage(is)),
-    ];
-    return CREImageList;
-  }
-  // Return ImageInfo structure if request does not have CRE label
-  else {
-    const imageInfoList = imageStreamList.map((imageStream) => {
-      return processImageInfo(imageStream);
-    });
-    return imageInfoList;
-  }
+    const imageStreamList = await Promise.resolve(getImageStreams(fastify, labels));
+    // Return CRE structured response if CRE label is included
+    if (labels['app.kubernetes.io/part-of'] === 'meteor-operator') {
+        const CREImageList: CREImageStreamDetails[] = [
+            ...imageStreamList.map((is) => mapImageStreamToCREImage(is)),
+        ];
+        return CREImageList;
+    }
+    // Return ImageInfo structure if request does not have CRE label
+    else {
+        const imageInfoList = imageStreamList.map((imageStream) => {
+            return processImageInfo(imageStream);
+        });
+        return imageInfoList;
+    }
 };
 
 const getImage = async (fastify: KubeFastifyInstance, imageName: string): Promise<ImageStream> => {
-  return fastify.kube.customObjectsApi
-    .getNamespacedCustomObject(
-      'image.openshift.io',
-      'v1',
-      fastify.kube.namespace,
-      'imagestreams',
-      imageName,
-    )
-    .then((res) => {
-      return res.body as ImageStream;
-    });
+    return fastify.kube.customObjectsApi
+        .getNamespacedCustomObject(
+            'image.openshift.io',
+            'v1',
+            fastify.kube.namespace,
+            'imagestreams',
+            imageName,
+        )
+        .then((res) => {
+            return res.body as ImageStream;
+        });
 };
 
 export const getImageInfo = async (
-  fastify: KubeFastifyInstance,
-  imageName: string,
+    fastify: KubeFastifyInstance,
+    imageName: string,
 ): Promise<ImageInfo> => {
-  return getImage(fastify, imageName).then((res) => {
-    return processImageInfo(res);
-  });
+    return getImage(fastify, imageName).then((res) => {
+        return processImageInfo(res);
+    });
 };
 
 const getImageStreams = async (
-  fastify: KubeFastifyInstance,
-  labels: { [key: string]: string },
+    fastify: KubeFastifyInstance,
+    labels: { [key: string]: string },
 ): Promise<ImageStream[]> => {
-  const labelString = convertLabelsToString(labels);
-  const requestPromise = fastify.kube.customObjectsApi
-    .listNamespacedCustomObject(
-      'image.openshift.io',
-      'v1',
-      fastify.kube.namespace,
-      'imagestreams',
-      undefined,
-      undefined,
-      undefined,
-      labelString,
-    )
-    .then((res) => {
-      const list = (
-        res?.body as {
-          items: ImageStream[];
-        }
-      ).items;
-      return list;
-    })
-    .catch((e) => {
-      fastify.log.error(e);
-      return [];
-    });
-  return await requestPromise;
+    const labelString = convertLabelsToString(labels);
+    const requestPromise = fastify.kube.customObjectsApi
+        .listNamespacedCustomObject(
+            'image.openshift.io',
+            'v1',
+            fastify.kube.namespace,
+            'imagestreams',
+            undefined,
+            undefined,
+            undefined,
+            labelString,
+        )
+        .then((res) => {
+            const list = (
+                res?.body as {
+                    items: ImageStream[];
+                }
+            ).items;
+            return list;
+        })
+        .catch((e) => {
+            fastify.log.error(e);
+            return [];
+        });
+    return await requestPromise;
 };
 
 export const processImageInfo = (imageStream: ImageStream): ImageInfo => {
-  const annotations = imageStream.metadata.annotations;
+    const annotations = imageStream.metadata.annotations;
 
-  const imageInfo: ImageInfo = {
-    name: imageStream.metadata.name,
-    description: annotations[IMAGE_ANNOTATIONS.DESC] || '',
-    url: annotations[IMAGE_ANNOTATIONS.URL] || '',
-    display_name: annotations[IMAGE_ANNOTATIONS.DISP_NAME] || imageStream.metadata.name,
-    tags: getTagInfo(imageStream),
-    order: +annotations[IMAGE_ANNOTATIONS.IMAGE_ORDER] || 100,
-    dockerImageRepo: imageStream.status?.dockerImageRepository || '',
-    labels: imageStream.metadata.labels,
-  };
+    const imageInfo: ImageInfo = {
+        name: imageStream.metadata.name,
+        description: annotations[IMAGE_ANNOTATIONS.DESC] || '',
+        url: annotations[IMAGE_ANNOTATIONS.URL] || '',
+        display_name: annotations[IMAGE_ANNOTATIONS.DISP_NAME] || imageStream.metadata.name,
+        tags: getTagInfo(imageStream),
+        order: +annotations[IMAGE_ANNOTATIONS.IMAGE_ORDER] || 100,
+        dockerImageRepo: imageStream.status?.dockerImageRepository || '',
+        labels: imageStream.metadata.labels,
+    };
 
-  return imageInfo;
+    return imageInfo;
 };
 
 // Check for existence in status.tags
 const checkTagExistence = (tag: ImageStreamTag, imageStream: ImageStream): boolean => {
-  if (imageStream.status) {
-    const tags = imageStream.status.tags;
-    if (tags) {
-      for (let i = 0; i < tags.length; i++) {
-        if (tags[i].tag === tag.name) {
-          return true;
+    if (imageStream.status) {
+        const tags = imageStream.status.tags;
+        if (tags) {
+            for (let i = 0; i < tags.length; i++) {
+                if (tags[i].tag === tag.name) {
+                    return true;
+                }
+            }
         }
-      }
     }
-  }
-  return false;
+    return false;
 };
 
 const getTagInfo = (imageStream: ImageStream): ImageTagInfo[] => {
-  const tagInfoArray: ImageTagInfo[] = [];
-  const tags = imageStream.spec.tags;
-  if (!tags?.length) {
-    console.error(`${imageStream.metadata.name} does not have any tags.`);
-    return;
-  }
-  tags.forEach((tag) => {
-    let tagAnnotations;
-    if (tag.annotations != null) {
-      tagAnnotations = tag.annotations;
-    } else {
-      tag.annotations = {};
-      tagAnnotations = {};
+    const tagInfoArray: ImageTagInfo[] = [];
+    const tags = imageStream.spec.tags;
+    if (!tags?.length) {
+        console.error(`${imageStream.metadata.name} does not have any tags.`);
+        return;
     }
-    if (!checkTagExistence(tag, imageStream)) {
-      return; //Skip tag
-    }
+    tags.forEach((tag) => {
+        let tagAnnotations;
+        if (tag.annotations != null) {
+            tagAnnotations = tag.annotations;
+        } else {
+            tag.annotations = {};
+            tagAnnotations = {};
+        }
+        if (!checkTagExistence(tag, imageStream)) {
+            return; //Skip tag
+        }
 
-    const tagInfo: ImageTagInfo = {
-      content: getTagContent(tag),
-      name: tag.name,
-      recommended: JSON.parse(tagAnnotations[IMAGE_ANNOTATIONS.RECOMMENDED] || 'false'),
-      default: JSON.parse(tagAnnotations[IMAGE_ANNOTATIONS.DEFAULT] || 'false'),
-      annotations: tagAnnotations,
-    };
-    tagInfoArray.push(tagInfo);
-  });
-  return tagInfoArray;
+        const tagInfo: ImageTagInfo = {
+            content: getTagContent(tag),
+            name: tag.name,
+            recommended: JSON.parse(tagAnnotations[IMAGE_ANNOTATIONS.RECOMMENDED] || 'false'),
+            default: JSON.parse(tagAnnotations[IMAGE_ANNOTATIONS.DEFAULT] || 'false'),
+            annotations: tagAnnotations,
+        };
+        tagInfoArray.push(tagInfo);
+    });
+    return tagInfoArray;
 };
 
 const getTagContent = (tag: ImageStreamTag): TagContent => {
-  const content: TagContent = {
-    software: JSON.parse(tag.annotations[IMAGE_ANNOTATIONS.SOFTWARE] || '[]'),
-    dependencies: JSON.parse(tag.annotations[IMAGE_ANNOTATIONS.DEPENDENCIES] || '[]'),
-  };
-  return content;
+    const content: TagContent = {
+        software: JSON.parse(tag.annotations[IMAGE_ANNOTATIONS.SOFTWARE] || '[]'),
+        dependencies: JSON.parse(tag.annotations[IMAGE_ANNOTATIONS.DEPENDENCIES] || '[]'),
+    };
+    return content;
 };
 
 const mapImageStreamToCREImage = (is: ImageStream): CREImageStreamDetails => ({
-  id: is.metadata.name,
-  hasImage: true,
-  name: is.metadata.annotations['opendatahub.io/notebook-image-name'],
-  description: is.metadata.annotations['opendatahub.io/notebook-image-desc'],
-  phase: is.metadata.annotations['opendatahub.io/notebook-image-phase'] as CREImageStreamPhase,
-  visible: is.metadata.labels['opendatahub.io/notebook-image'] === 'true',
-  error: is.metadata.annotations['opendatahub.io/notebook-image-messages']
-    ? JSON.parse(is.metadata.annotations['opendatahub.io/notebook-image-messages'])
-    : [],
-  packageAnnotations:
-    is.spec.tags &&
-    (JSON.parse(
-      is.spec.tags[0].annotations['opendatahub.io/notebook-python-dependencies'],
-    ) as CREPackageAnnotation[]),
-  softwareAnnotations:
-    is.spec.tags &&
-    (JSON.parse(
-      is.spec.tags[0].annotations['opendatahub.io/notebook-software'],
-    ) as CREPackageAnnotation[]),
-  uploaded: is.metadata.creationTimestamp,
-  url: is.metadata.annotations['opendatahub.io/notebook-image-url'],
-  user: is.metadata.annotations['opendatahub.io/notebook-image-creator'],
-  labels: is.metadata.labels,
+    id: is.metadata.name,
+    hasImage: true,
+    name: is.metadata.annotations['opendatahub.io/notebook-image-name'],
+    description: is.metadata.annotations['opendatahub.io/notebook-image-desc'],
+    phase: is.metadata.annotations['opendatahub.io/notebook-image-phase'] as CREImageStreamPhase,
+    visible: is.metadata.labels['opendatahub.io/notebook-image'] === 'true',
+    error: is.metadata.annotations['opendatahub.io/notebook-image-messages']
+        ? JSON.parse(is.metadata.annotations['opendatahub.io/notebook-image-messages'])
+        : [],
+    packageAnnotations:
+        is.spec.tags &&
+        (JSON.parse(
+            is.spec.tags[0].annotations['opendatahub.io/notebook-python-dependencies'],
+        ) as CREPackageAnnotation[]),
+    softwareAnnotations:
+        is.spec.tags &&
+        (JSON.parse(
+            is.spec.tags[0].annotations['opendatahub.io/notebook-software'],
+        ) as CREPackageAnnotation[]),
+    uploaded: is.metadata.creationTimestamp,
+    url: is.metadata.annotations['opendatahub.io/notebook-image-url'],
+    user: is.metadata.annotations['opendatahub.io/notebook-image-creator'],
+    labels: is.metadata.labels,
 });
 
 export const deleteImage = async (
-  fastify: KubeFastifyInstance,
-  request: FastifyRequest,
+    fastify: KubeFastifyInstance,
+    request: FastifyRequest,
 ): Promise<{ success: boolean; error: string }> => {
-  const customObjectsApi = fastify.kube.customObjectsApi;
-  const namespace = fastify.kube.namespace;
-  const params = request.params as { image: string };
+    const customObjectsApi = fastify.kube.customObjectsApi;
+    const namespace = fastify.kube.namespace;
+    const params = request.params as { image: string };
 
-  try {
-    await customObjectsApi
-      .deleteNamespacedCustomObject(
-        'image.openshift.io',
-        'v1',
-        namespace,
-        'imagestreams',
-        params.image,
-      )
-      .catch((e) => {
-        throw createError(e.statusCode, e?.body?.message);
-      });
-    return { success: true, error: null };
-  } catch (e) {
-    if (e.response?.statusCode !== 404) {
-      fastify.log.error('Unable to delete notebook image: ' + e.toString());
-      return { success: false, error: 'Unable to delete notebook image: ' + e.message };
+    try {
+        await customObjectsApi
+            .deleteNamespacedCustomObject(
+                'image.openshift.io',
+                'v1',
+                namespace,
+                'imagestreams',
+                params.image,
+            )
+            .catch((e) => {
+                throw createError(e.statusCode, e?.body?.message);
+            });
+        return { success: true, error: null };
+    } catch (e) {
+        if (e.response?.statusCode !== 404) {
+            fastify.log.error('Unable to delete notebook image: ' + e.toString());
+            return { success: false, error: 'Unable to delete notebook image: ' + e.message };
+        }
     }
-  }
 };
 
 export const updateImage = async (
-  fastify: KubeFastifyInstance,
-  request: FastifyRequest,
+    fastify: KubeFastifyInstance,
+    request: FastifyRequest,
 ): Promise<{ success: boolean; error: string }> => {
-  const customObjectsApi = fastify.kube.customObjectsApi;
-  const namespace = fastify.kube.namespace;
-  const params = request.params as { image: string };
-  const body = request.body as CREImageStreamUpdateRequest;
-  const labels = {
-    'app.kubernetes.io/created-by': 'cre',
-    'opendatahub.io/notebook-image': 'true',
-  };
+    const customObjectsApi = fastify.kube.customObjectsApi;
+    const namespace = fastify.kube.namespace;
+    const params = request.params as { image: string };
+    const body = request.body as CREImageStreamUpdateRequest;
+    const labels = {
+        'app.kubernetes.io/created-by': 'cre',
+        'opendatahub.io/notebook-image': 'true',
+    };
 
-  const imageStreams = await getImageStreams(fastify, labels);
-  const validName = imageStreams.filter(
-    (is) =>
-      is.metadata.annotations['opendatahub.io/notebook-image-name'] === body.name &&
-      is.metadata.name !== body.id,
-  );
+    const imageStreams = await getImageStreams(fastify, labels);
+    const validName = imageStreams.filter(
+        (is) =>
+            is.metadata.annotations['opendatahub.io/notebook-image-name'] === body.name &&
+            is.metadata.name !== body.id,
+    );
 
-  if (validName.length > 0) {
-    fastify.log.error('Duplicate name unable to add notebook image');
-    return { success: false, error: 'Unable to add notebook image: ' + body.name };
-  }
-
-  try {
-    const imageStream = await customObjectsApi
-      .getNamespacedCustomObject(
-        'image.openshift.io',
-        'v1',
-        namespace,
-        'imagestreams',
-        params.image,
-      )
-      .then((r) => r.body as ImageStream)
-      .catch((e) => {
-        throw createError(e.statusCode, e?.body?.message);
-      });
-
-    if (body.packageAnnotations && imageStream.spec.tags) {
-      imageStream.spec.tags[0].annotations['opendatahub.io/notebook-python-dependencies'] =
-        JSON.stringify(body.packageAnnotations);
+    if (validName.length > 0) {
+        fastify.log.error('Duplicate name unable to add notebook image');
+        return { success: false, error: 'Unable to add notebook image: ' + body.name };
     }
 
-    if (body.softwareAnnotations && imageStream.spec.tags) {
-      imageStream.spec.tags[0].annotations['opendatahub.io/notebook-software'] = JSON.stringify(
-        body.softwareAnnotations,
-      );
-    }
+    try {
+        const imageStream = await customObjectsApi
+            .getNamespacedCustomObject(
+                'image.openshift.io',
+                'v1',
+                namespace,
+                'imagestreams',
+                params.image,
+            )
+            .then((r) => r.body as ImageStream)
+            .catch((e) => {
+                throw createError(e.statusCode, e?.body?.message);
+            });
 
-    if (typeof body.visible !== undefined) {
-      if (body.visible) {
-        imageStream.metadata.labels['opendatahub.io/notebook-image'] = 'true';
-      } else {
-        imageStream.metadata.labels['opendatahub.io/notebook-image'] = 'false';
-      }
-    }
-    if (body.name) {
-      imageStream.metadata.annotations['opendatahub.io/notebook-image-name'] = body.name;
-    }
+        if (body.packageAnnotations && imageStream.spec.tags) {
+            imageStream.spec.tags[0].annotations['opendatahub.io/notebook-python-dependencies'] =
+                JSON.stringify(body.packageAnnotations);
+        }
 
-    if (body.description !== undefined) {
-      imageStream.metadata.annotations['opendatahub.io/notebook-image-desc'] = body.description;
-    }
+        if (body.softwareAnnotations && imageStream.spec.tags) {
+            imageStream.spec.tags[0].annotations['opendatahub.io/notebook-software'] = JSON.stringify(
+                body.softwareAnnotations,
+            );
+        }
 
-    await customObjectsApi
-      .patchNamespacedCustomObject(
-        'image.openshift.io',
-        'v1',
-        namespace,
-        'imagestreams',
-        params.image,
-        imageStream,
-        undefined,
-        undefined,
-        undefined,
-        {
-          headers: { 'Content-Type': 'application/merge-patch+json' },
-        },
-      )
-      .catch((e) => console.error(e));
+        if (typeof body.visible !== undefined) {
+            if (body.visible) {
+                imageStream.metadata.labels['opendatahub.io/notebook-image'] = 'true';
+            } else {
+                imageStream.metadata.labels['opendatahub.io/notebook-image'] = 'false';
+            }
+        }
+        if (body.name) {
+            imageStream.metadata.annotations['opendatahub.io/notebook-image-name'] = body.name;
+        }
 
-    return { success: true, error: null };
-  } catch (e) {
-    if (e.response?.statusCode !== 404) {
-      fastify.log.error('Unable to update notebook image: ' + e.toString());
-      return { success: false, error: 'Unable to update notebook image: ' + e.message };
+        if (body.description !== undefined) {
+            imageStream.metadata.annotations['opendatahub.io/notebook-image-desc'] = body.description;
+        }
+
+        await customObjectsApi
+            .patchNamespacedCustomObject(
+                'image.openshift.io',
+                'v1',
+                namespace,
+                'imagestreams',
+                params.image,
+                imageStream,
+                undefined,
+                undefined,
+                undefined,
+                {
+                    headers: { 'Content-Type': 'application/merge-patch+json' },
+                },
+            )
+            .catch((e) => console.error(e));
+
+        return { success: true, error: null };
+    } catch (e) {
+        if (e.response?.statusCode !== 404) {
+            fastify.log.error('Unable to update notebook image: ' + e.toString());
+            return { success: false, error: 'Unable to update notebook image: ' + e.message };
+        }
     }
-  }
 };
--- backend/src/routes/api/images/index.ts
+++ backend/src/routes/api/images/index.ts
@@ -4,53 +4,53 @@ import { deleteImage, getImageList, updateImage } from './imageUtils';
 import { secureAdminRoute, secureRoute } from '../../../utils/route-security';
 
 export default async (fastify: FastifyInstance): Promise<void> => {
-  fastify.get(
-    '/:type',
-    secureRoute(fastify)(async (request: FastifyRequest, reply: FastifyReply) => {
-      let labels = {};
-      const params = request.params as { type: ImageType };
-      if (params.type === 'cre') {
-        labels = {
-          'app.kubernetes.io/created-by': 'cpe-_a-meteor.zone-CNBi-v0.1.0',
-        };
-      } else {
-        labels = {
-          'opendatahub.io/notebook-image': 'true',
-        };
-      }
-      return getImageList(fastify, labels)
-        .then((res) => {
-          return res;
-        })
-        .catch((res) => {
-          reply.send(res);
-        });
-    }),
-  );
+    fastify.get(
+        '/:type',
+        secureRoute(fastify)(async (request: FastifyRequest, reply: FastifyReply) => {
+            let labels = {};
+            const params = request.params as { type: ImageType };
+            if (params.type === 'cre') {
+                labels = {
+                    'app.kubernetes.io/part-of': 'meteor-operator',
+                };
+            } else {
+                labels = {
+                    'opendatahub.io/notebook-image': 'true',
+                };
+            }
+            return getImageList(fastify, labels)
+                .then((res) => {
+                    return res;
+                })
+                .catch((res) => {
+                    reply.send(res);
+                });
+        }),
+    );
 
-  fastify.delete(
-    '/:image',
-    secureAdminRoute(fastify)(async (request: FastifyRequest, reply: FastifyReply) => {
-      return deleteImage(fastify, request)
-        .then((res) => {
-          return res;
-        })
-        .catch((res) => {
-          reply.send(res);
-        });
-    }),
-  );
+    fastify.delete(
+        '/:image',
+        secureAdminRoute(fastify)(async (request: FastifyRequest, reply: FastifyReply) => {
+            return deleteImage(fastify, request)
+                .then((res) => {
+                    return res;
+                })
+                .catch((res) => {
+                    reply.send(res);
+                });
+        }),
+    );
 
-  fastify.put(
-    '/:image',
-    secureAdminRoute(fastify)(async (request: FastifyRequest, reply: FastifyReply) => {
-      return updateImage(fastify, request)
-        .then((res) => {
-          return res;
-        })
-        .catch((res) => {
-          reply.send(res);
-        });
-    }),
-  );
+    fastify.put(
+        '/:image',
+        secureAdminRoute(fastify)(async (request: FastifyRequest, reply: FastifyReply) => {
+            return updateImage(fastify, request)
+                .then((res) => {
+                    return res;
+                })
+                .catch((res) => {
+                    reply.send(res);
+                });
+        }),
+    );
 };
